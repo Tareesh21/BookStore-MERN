de9630f7fba418fca13c6d7dea7e1b573e595bd2
Tareesh2000 <tareeshreddy1@gmail.com>
2024-11-12 14:32:53 -0800

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://tareesh21:tareesh21@book-store-mern.1tvzt.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN';
